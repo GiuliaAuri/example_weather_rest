2f3883687aea5cb116280ec671ee76e5e951f485
GiuliaAuri <314739@studenti.unimore.it>
2024-07-19 12:58:10 +0200

    Update

--- src/main/java/com/weather_app/WeatherApp.java
+++ src/main/java/com/weather_app/WeatherApp.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class WeatherApp extends Application{
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(WeatherApp.class.getResource("weather-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(WeatherApp.class.getResource("/com/weather_app/weather-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1050, 625);
         stage.setTitle("Weather Application");
         stage.setScene(scene);
